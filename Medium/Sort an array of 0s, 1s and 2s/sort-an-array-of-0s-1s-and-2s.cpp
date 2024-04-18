1f386ce9b3d5386f747d9f50d1a3190b9553d0ba
Santhosh <santhoshhariharan1393@gmail.com>
2024-04-18 17:20:52 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
+++ Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
@@ -6,12 +6,31 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution
-{
+{ 
     public:
     void sort012(int a[], int n)
     {
         // code here 
-        sort(a,a+n);
+        int l=0;
+        int m=0;
+        int h=n-1;
+        while(m<=h){
+            if(a[m]==0){
+                swap(a[l],a[m]);
+                l++;
+                m++;
+               
+            }
+            else if(a[m]==1){
+                m++;
+                
+            }
+            else{
+                swap(a[m],a[h]);
+                h--;
+           
+            }
+        }
         
     }
     
