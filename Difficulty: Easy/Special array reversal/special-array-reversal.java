a3b08124d2a9aef60a49e887e553a0d478e4abfa
Santhosh <santhoshhariharan1393@gmail.com>
2024-06-26 16:17:46 +0530

    Update special-array-reversal.java

--- Difficulty: Easy/Special array reversal/special-array-reversal.java
+++ Difficulty: Easy/Special array reversal/special-array-reversal.java
@@ -28,18 +28,15 @@ class Solution
         //complete the function here
         char[] arr = str.toCharArray();
         int left = 0, right = arr.length - 1;
-
         while (left < right) {
-            // Move left pointer to the next alphabet character
-            while (left < right && !Character.isLetter(arr[left])) {
+            if (!Character.isLetter(arr[left])) {
                 left++;
-            }
-            // Move right pointer to the previous alphabet character
-            while (left < right && !Character.isLetter(arr[right])) {
+            } 
+            else if (!Character.isLetter(arr[right])) {
                 right--;
-            }
-            // Swap the alphabet characters at left and right pointers
-            if (left < right) {
+            } 
+            else {
+            // Swap alphabet characters at left and right pointers
                 char temp = arr[left];
                 arr[left] = arr[right];
                 arr[right] = temp;
