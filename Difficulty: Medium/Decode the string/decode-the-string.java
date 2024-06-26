cfcc54900a678f9bf82fdfb662adc0ab4e033f7d
Santhosh <santhoshhariharan1393@gmail.com>
2024-06-26 16:26:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String s = in.readLine();
+            
+            Solution ob = new Solution();
+            out.println(ob.decodedString(s));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String decodedString(String s){
+        // code here
+         Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int currentNumber = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                currentNumber = currentNumber * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(currentNumber);
+                stringStack.push(currentString);
+                currentNumber = 0;
+                currentString = new StringBuilder();
+            } else if (ch == ']') {
+                StringBuilder decodedSubstring = currentString;
+                currentString = stringStack.pop();
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    currentString.append(decodedSubstring);
+                }
+            } else {
+                currentString.append(ch);
+            }
+        }
+        
+        return currentString.toString();
+    }
+}
