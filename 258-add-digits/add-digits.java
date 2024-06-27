723e20e01578092dc01f1e262ee02e90e877d03e
Santhosh <santhoshhariharan1393@gmail.com>
2024-06-27 21:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (23.13%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+//Explanation of the Code:
+// Base Case: If the number is zero, directly return zero.
+// Digital Root Calculation: For any other number, use the formula 
+// 1+(num−1)%9 to find the digital root.
+// Why Does the Formula Work?
+// 1+(num−1)%9 is derived from properties of numbers in modular arithmetic. 
+// It effectively compresses any number down to its single-digit sum by
+// utilizing the periodicity of digits in base-10. 
+//The result of this formula is always a single-digit number that is the sum of the digits of the original number.
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            return 1 + (num - 1) % 9;
+        }
+    }
+}
