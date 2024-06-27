f9fabe1835e7b40e77c54e42e35346f9e431aff8
Santhosh <santhoshhariharan1393@gmail.com>
2024-06-27 14:42:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word Break/word-break.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+public class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n;
+                    n = sc.nextInt();
+                    ArrayList<String> dictionary = new ArrayList<String>();
+                    for(int i = 0;i<n;i++)
+                        {
+                            String p = sc.next();
+                            dictionary.add(p);
+                        }
+                    String s = sc.next();
+                    Solution obj = new Solution();  
+                    System.out.println(obj.wordBreak(n, s, dictionary));  
+                    
+                }
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+     public static int wordBreak(int n, String s, ArrayList<String> dictionary) {
+        Map<String, Boolean> map = new HashMap<>();
+        return wordBreakHelper(s, dictionary, map) ? 1 : 0;
+    }
+
+    private static boolean wordBreakHelper(String s, ArrayList<String> dictionary, Map<String, Boolean> map) {
+        if (dictionary.contains(s))
+            return true;
+        if (map.containsKey(s))
+            return map.get(s);
+        for (int i = 1; i <= s.length(); i++) {
+            String left = s.substring(0, i);
+            if (dictionary.contains(left) && wordBreakHelper(s.substring(i), dictionary, map)) {
+                map.put(s, true);
+                return true;
+            }
+        }
+        map.put(s, false);
+        return false;
+    }
+    
+}
