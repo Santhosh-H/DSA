cbe64fd618243ced2d29c4c480b95b584613dc85
Santhosh <santhoshhariharan1393@gmail.com>
2024-06-28 21:48:05 +0530

    Time: 1 ms (37.32%) | Memory: 44 MB (65.19%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -5,6 +5,7 @@ class Solution {
         int m=mat[0].length;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                //i+j==n-1 is to find the secondary diaongal
                 if( i==j || (i+j==n-1)){
                     ans=ans+mat[i][j];
                 }
