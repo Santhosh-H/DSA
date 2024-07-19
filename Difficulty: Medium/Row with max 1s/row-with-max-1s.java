a73ac06687e4eea096b07ab14b548ea08e07987c
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-19 20:48:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int lowerBound(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                // look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+        
+    }
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        int cnt_max = 0;
+        int index = -1;
+        int n=arr.length;
+        int m=arr[0].length;
+        // traverse the rows:
+        for (int i = 0; i < n; i++) {
+            // get the number of 1's:
+            int cnt_ones = m - lowerBound(arr[i], m, 1);
+            if (cnt_ones > cnt_max) {
+                cnt_max = cnt_ones;
+                index = i;
+            }
+        }
+        return index;
+    }
+}
