31033c306c1562e30136278c324843839c11d8f4
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-20 12:56:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class KthSmallestElement{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=Integer.parseInt(sc.next());
+		while(t>0)
+		{
+			int n=Integer.parseInt(sc.next());
+			int[][] a=new int[n][n];
+			for(int i=0;i<n;i++)
+				for(int j=0;j<n;j++)
+    				a[i][j]=Integer.parseInt(sc.next());
+    		
+			int k=Integer.parseInt(sc.next());
+		    Solution ob = new Solution();
+		    System.out.println(ob.kthSmallest(a,n,k));
+		    
+		    t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+class Solution
+{
+	public static int kthSmallest(int[][]mat,int n,int k)
+	{
+        //code here.
+         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+           for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            pq.add(mat[i][j]);
+            
+            // If the size of the heap exceeds k, remove the largest element
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+    }
+    
+    // The root of the max-heap will be the k-th smallest element
+    return pq.peek();
+    }
+}
