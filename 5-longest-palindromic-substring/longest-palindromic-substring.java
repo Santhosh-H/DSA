7a11ecec7aa0c0545e056e4ba7c9902552e6d81a
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-22 11:54:22 +0530

    Time: 16 ms (77.08%) | Memory: 41.9 MB (88.42%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -4,9 +4,8 @@ class Solution {
         int max_len = 1;
         int n = s.length();
         int st = 0, end = 0;
-        
         // Odd length
-        for(int i = 0; i < n-1; ++i){
+        for(int i = 0; i < n; i++){
             int l = i, r = i;
             while(l >= 0 && r < n){
                 if(s.charAt(l) == s.charAt(r)){
@@ -23,7 +22,7 @@ class Solution {
         }
         
         // Even length
-        for(int i = 0; i < n-1; ++i){
+        for(int i = 0; i < n; i++){
             int l = i, r = i+1;
             while(l >= 0 && r < n){
                 if(s.charAt(l) == s.charAt(r)){
