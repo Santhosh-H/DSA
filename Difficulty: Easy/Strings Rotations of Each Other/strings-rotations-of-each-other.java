97f6f68647547744fff9a236a1f15593375c8193
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-22 21:34:32 +0530

    Update strings-rotations-of-each-other.java

--- Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.java
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -42,67 +42,22 @@ class GFG {
 
 class Solution
 {
-     // Function to implement KMP algorithm to check if pattern is a substring of text
-    private static boolean kmpSearch(String text, String pattern) {
-        int[] lps = buildLPS(pattern);
-        int i = 0, j = 0;
-        
-        while (i < text.length()) {
-            if (pattern.charAt(j) == text.charAt(i)) {
-                i++;
-                j++;
-            }
-            if (j == pattern.length()) {
-                return true; // Found pattern
-            } else if (i < text.length() && pattern.charAt(j) != text.charAt(i)) {
-                if (j != 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
-            }
-        }
-        
-        return false;
-    }
-
-    // Function to build the LPS (Longest Prefix which is also Suffix) array for KMP algorithm
-    private static int[] buildLPS(String pattern) {
-        int[] lps = new int[pattern.length()];
-        int length = 0;
-        int i = 1;
-        
-        while (i < pattern.length()) {
-            if (pattern.charAt(i) == pattern.charAt(length)) {
-                length++;
-                lps[i] = length;
-                i++;
-            } else {
-                if (length != 0) {
-                    length = lps[length - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-        
-        return lps;
-    }
-
     //Function to check if two strings are rotations of each other or not.
     public static boolean areRotations(String s1, String s2 )
     {
-        // Your code here
-         if (s1.length() != s2.length()) {
-            return false;
+         String str = s1 + s1;
+        int i=0; int j=0;
+        while(i<str.length() && j<s2.length()){
+            if(str.charAt(i) == s2.charAt(j)){
+                i++;
+                j++;
+            }else{
+                i++;
+            }
         }
         
-        // Concatenate s1 with itself
-        String concatenated = s1 + s1;
-        
-        // Use KMP to check if s2 is a substring of concatenated
-        return kmpSearch(concatenated, s2);
+        return j==s2.length();
+    
     }
     
 }
