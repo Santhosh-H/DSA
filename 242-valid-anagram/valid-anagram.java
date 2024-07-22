1847e40b63cf00ba2237b75e314e4f7ff244ec3c
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-22 19:48:32 +0530

    Time: 6 ms (42.31%) | Memory: 43.3 MB (63.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,25 +1,45 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+    //     if (s.length() != t.length()) {
+    //     return false;
+    // }
+
+    // // Convert strings to character arrays
+    // char[] in = s.toCharArray();
+    // char[] check = t.toCharArray();
+
+    // // Sort the character arrays
+    // Arrays.sort(in);
+    // Arrays.sort(check);
+
+    // // Compare the sorted arrays
+    // for (int i = 0; i < in.length; i++) {
+    //     if (in[i] != check[i]) {
+    //         return false;
+    //     }
+    // }
+
+    // return true;
+   if (s.length() != t.length()) {
         return false;
     }
 
-    // Convert strings to character arrays
-    char[] in = s.toCharArray();
-    char[] check = t.toCharArray();
+    // Create a count array for each character (since they are lowercase English letters)
+    int[] count = new int[26];
 
-    // Sort the character arrays
-    Arrays.sort(in);
-    Arrays.sort(check);
+    // Count characters in both strings
+    for (int i = 0; i < s.length(); i++) {
+        count[s.charAt(i) - 'a']++;
+        count[t.charAt(i) - 'a']--;
+    }
 
-    // Compare the sorted arrays
-    for (int i = 0; i < in.length; i++) {
-        if (in[i] != check[i]) {
+    // Check if all counts are zero
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
             return false;
         }
     }
 
     return true;
-        
-    }
+}
 }
