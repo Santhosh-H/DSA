f088c1e41e3f242be65cf71ce88a9b6a845168ac
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-22 12:11:51 +0530

    Time: 15 ms (88.09%) | Memory: 41.8 MB (93.48%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,32 +1,44 @@
 class Solution {
-    public static String longestPalindromeOptimal(String s) {
+     public static String longestPalindromeOptimal(String s) {
         if (s == null || s.length() < 1) return "";
         int start = 0, end = 0;
 
+        // Iterate through each character in the string
         for (int i = 0; i < s.length(); i++) {
+            // Check for odd-length palindromes
             int len1 = expandAroundCenter(s, i, i);
+            // Check for even-length palindromes
             int len2 = expandAroundCenter(s, i, i + 1);
+            // Get the maximum length from the two cases
             int len = Math.max(len1, len2);
 
+            // If the found palindrome is longer than the current longest, update start and end
             if (len > end - start) {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
         }
 
+        // Return the longest palindromic substring
         return s.substring(start, end + 1);
     }
 
     private static int expandAroundCenter(String s, int left, int right) {
+        // Expand as long as the characters on both sides are equal
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
+        // Return the length of the palindrome
         return right - left - 1;
     }
 
     public String longestPalindrome(String s) {
         return longestPalindromeOptimal(s);
+        
+        
+        
+        
         // if(s.length() <= 1) return s;
         // int max_len = 1;
         // int n = s.length();
