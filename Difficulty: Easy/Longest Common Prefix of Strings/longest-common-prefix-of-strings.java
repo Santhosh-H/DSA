4e215c9ba01c4fd289ea308627321d276a8ded28
Santhosh <santhoshhariharan1393@gmail.com>
2024-07-24 21:01:50 +0530

    Update longest-common-prefix-of-strings.java

--- Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.java
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.java
@@ -24,21 +24,38 @@ class GFG {
 class Solution {
     public String longestCommonPrefix(String arr[]) {
         // code here
-         if (arr == null || arr.length == 0) {
-            return "-1";
-        }
+        //  if (arr == null || arr.length == 0) {
+        //     return "-1";
+        // }
         
-        String prefix = arr[0];
+        // String prefix = arr[0];
         
-        for (int i = 1; i < arr.length; i++) {
-            while (arr[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
-                    return "-1";
-                }
-            }
-        }
+        // for (int i = 1; i < arr.length; i++) {
+        //     while (arr[i].indexOf(prefix) != 0) {
+        //         prefix = prefix.substring(0, prefix.length() - 1);
+        //         if (prefix.isEmpty()) {
+        //             return "-1";
+        //         }
+        //     }
+        // }
         
-        return prefix;
+        // return prefix;
+// youtube approach 
+    StringBuilder result = new StringBuilder();
+    // Sort the array
+    Arrays.sort(arr);
+    // Get the first and last strings
+    char[] first = arr[0].toCharArray();
+    char[] last = arr[arr.length - 1].toCharArray();
+
+    // Start comparing
+    for (int i = 0; i < first.length; i++) {
+      if (first[i] != last[i])
+        break;
+      result.append(first[i]);
+    }
+    if(result.length()==0) result.append("-1");
+    return result.toString();
+    
     }
 }
