1eeade89d15588fcf5cf9f8b5571433f853ce304
Santhosh <santhoshhariharan1393@gmail.com>
2024-09-09 20:13:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Factors/prime-factors.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int[] ans = ob.AllPrimeFactors(N);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    public int[] AllPrimeFactors(int N)
+    {
+        // code here
+        List<Integer> l=new ArrayList<Integer>();
+        for(int i=2;i<=N;i++){
+            if(N%i==0){
+                l.add(i);
+                while(N%i==0){
+                    N=N/i;
+                }
+            }
+        }
+        int[] arr = new int[l.size()];
+        for (int i = 0; i < l.size(); i++) {
+            arr[i] = l.get(i);  // Transfer elements from the list to the array
+        }
+
+        return arr;
+    }
+}
